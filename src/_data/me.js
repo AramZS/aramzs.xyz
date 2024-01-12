013fc8c73ac8819e2d4a8d58af286538af6b1259
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2024-01-12 01:02:10 -0500

    Getting basic build working

---
+++ src/_data/me.js
@@ -0,0 +1,10 @@
+let data = {
+	name: "Aram Zucker-Scharff",
+	aboutMePage: "https://aramzs.github.io/aramzs/",
+};
+
+data.avatar = `/img/twitter-avy.jpg`;
+
+module.exports = (info) => {
+	return data;
+};
