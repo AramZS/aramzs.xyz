8a3b89a31082b3b71f2a8d750b5cf7a3926db3b6
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2024-02-04 13:09:46 -0500

    Cleaning out some data and templates that are particular to the origional author. Setting up a terms file

--- src/_data/quotes.js
+++ src/_data/quotes.js
@@ -1,121 +1,27 @@
 module.exports = [
-  {
-    blockquote: "Everything not saved will be lost",
-    cite: 'Nintendo Quit Screen',
-  },
-  {
-    blockquote: "To use a history of discrimination to deny people their constitutional rights is a perversion of logic and a betrayal of justice.",
-    cite: 'Sarah Grimké',
-  },
-  {
-    blockquote: "Nations that go down fighting rise again, but those who surrender tamely are finished.",
-    cite: 'Churchill',
-  },
-  {
-    blockquote: "The world is changed by your example, not by your opinion",
-    cite: 'Paulo Coelho',
-  },
-  {
-    blockquote: "Thinking is difficult, that's why most people judge",
-    cite: 'Carl Gustav Jung',
-  },
-  {
-    blockquote: "More than cleverness, we need kindness and gentleness",
-    cite: 'Charlie Chaplin',
-  },
-  {
-    blockquote: "Freedom is the only worthy goal in life. It is won by disregarding things that lie beyond our control.",
-    cite: 'Epictetus',
-  },
-  {
-    blockquote: "Human beings are born with different capacities. If they are free, they are not equal. And if they are equal, they are not free.",
-    cite: 'Aleksandr Solzhenitsyn',
-  },
+	{
+		blockquote: "Everything not saved will be lost",
+		cite: "Nintendo Quit Screen",
+	},
+	{
+		blockquote:
+			"What labor wants is land for the landless, produce to the producer, tools to the toiler—and death to wage slavery.",
+		cite: "Lucy Parsons",
+	},
+	{
+		blockquote:
+			"We must have money; a father’s debts are to be paid, an aged mother to be supported, a brother’s ambition to be aided and so the factories are supplied. Is this to act from free will? Is this freedom? To my mind it is slavery.",
+		cite: "Sarah Bagley",
+	},
+	{
+		blockquote:
+			"Surveillance capitalism unilaterally claims human experience as free raw material for translation into behavioral data.",
+		cite: "Shoshana Zuboff",
+	},
+	//
 
-  {
-    blockquote: "Raise your words, not voice. It is rain that grows flowers, not thunder.",
-    cite: 'Rumi',
-  },
-
-  {
-    blockquote: "Imperfection is beauty, madness is genius, and it's better to be absolutely ridiculous than absolutely boring.",
-    cite: 'Marilyn Monroe',
-  },
-
-  {
-    blockquote: "It is better to be hated for what you are than to be loved for what you are not.",
-    cite: 'Andre Gide',
-  },
-
-  {
-    blockquote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
-    cite: 'Martin Luther King Jr',
-  },
-
-  {
-    blockquote: "Wealth consists not in having great possessions, but in having few wants.",
-    cite: 'Epictetus',
-  },
-
-  {
-    blockquote: "We carry inside us the wonders we seek outside us.",
-    cite: 'Rumi',
-  },
-
-  {
-    blockquote: "The beginning is the most important part of the work.",
-    cite: 'Plato',
-  },
-
-  {
-    blockquote: "To me programming is more than an important practical art.",
-    cite: 'Grace Hopper',
-  },
-
-  {
-    blockquote: "We mature with the damage, not with the years.",
-    cite: 'Mateus Williams',
-  },
-
-  {
-    blockquote: "When a person can't find a deep sense of meaning , they distract themselves with pleasure.",
-    cite: 'Viktor Frankl',
-  },
-
-  {
-    blockquote: "Failure is a bruise, not a tattoo",
-    cite: 'Jon Sinclair',
-  },
-
-  {
-    blockquote: "Write it on your heart that every day is the best day in the year.",
-    cite: 'Ralph Waldo',
-  },
-
-  {
-    blockquote: "We all have 365 days every year. The difference is what you do with them.",
-    cite: 'Hillary DePiano',
-  },
-
-  {
-    blockquote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    cite: 'Martin Fowler',
-  },
-
-  {
-    blockquote: "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.",
-    cite: 'Dan Salomon',
-  },
-
-  {
-    blockquote: "Your children need your presence more than your presents.",
-    cite: 'Jesse Jackson',
-  },
-
-  //
-
-  // {
-  //   blockquote: "",
-  //   cite: '',
-  // },
+	// {
+	//   blockquote: "",
+	//   cite: '',
+	// },
 ];
---
+++ src/_data/words.js
@@ -0,0 +1,135 @@
+module.exports = [
+	{
+		term: "neofeudalism",
+		definition:
+			"the consolidation of elite wealth and power far beyond the control of ordinary people and the mechanisms of democratic consent.",
+		cite: "The Age of Surveillance Capitalism by Shoshana Zuboff",
+	},
+	{
+		term: "patrimonial capitalism",
+		definition:
+			"a reversion to a premodern society in which one’s life chances depend upon inherited wealth rather than meritocratic achievement.",
+		cite: "The Age of Surveillance Capitalism by Shoshana Zuboff",
+	},
+	{
+		term: "Cynosure",
+		definition: "A person or thing that is the center of attention",
+	},
+	{
+		term: "Agnotology",
+		definition:
+			"the study of deliberate, culturally induced ignorance or doubt, typically to sell a product, influence opinion, or win favour, particularly through the publication of inaccurate or misleading scientific data (disinformation).",
+	},
+	{
+		term: "lenticular",
+		definition: "shaped like a lentil or relating the the lens of an eye.",
+	},
+	{
+		term: "Brobdingnagian",
+		definition: "enormous, gigantic, of colossal size.",
+	},
+	{
+		term: "ibogaine",
+		definition: "a hallucinogenic compound ",
+	},
+	{
+		term: "tilth",
+		definition: "the cultivation of land.",
+	},
+	{
+		term: "Loglines",
+		definition:
+			"a brief (usually one-sentence) summary of a television program, film, short film, or book that states the central conflict of the story",
+	},
+	{
+		term: "chiral",
+		definition:
+			"asymmetric in such a way that the structure and its mirror image are not superimposable. Chiral compounds are typically optically active; large organic molecules often have one or more chiral centers where four different groups are attached to a carbon atom.",
+	},
+	{
+		term: "threnody",
+		definition: "a lament, dirge, requiem",
+	},
+	{
+		term: "tendentious",
+		definition:
+			"Marked by or favoring a particular point of view; partisan. Having a tendency; written or spoken with a partisan, biased or prejudiced purpose. Implicitly or explicitly slanted.",
+	},
+	{
+		term: "immiseration",
+		definition: "economic impoverishment",
+	},
+	{
+		term: "shibboleth",
+		definition:
+			"a custom or usage regarded as distinguishing one group from others ",
+	},
+	{
+		term: "Collywobbles",
+		definition: "a feeling of fear, apprehension, nervousness",
+	},
+	{
+		term: "clerestory",
+		definition:
+			"An upper portion of a wall containing windows for supplying natural light to a building.",
+	},
+	{
+		term: "Atopy",
+		definition:
+			"A hereditary disorder marked by the tendency to develop localized immediate hypersensitivity reactions to allergens such as pollen food, etc...",
+	},
+	{
+		term: "Pize",
+		definition: "to curse or strike someone",
+	},
+	{
+		term: "Tec",
+		definition: "a detective",
+	},
+	{
+		term: "nim",
+		definition:
+			"A game in which players in turn remove small objects from a collection, such as matchsticks arranged in rows, and attempt to take, or avoid taking, the last one. / To Take, To Steal, To Filch",
+	},
+	{
+		term: "manes",
+		definition:
+			"The souls or spirits of dead ancestors, conceived as deities or the subjects of reverence.",
+	},
+	{
+		term: "rax",
+		definition:
+			"To perform the act of reaching or stretching; stretch one's self; reach for or try to obtain something",
+	},
+	{
+		term: "rive",
+		definition: "to rend, tear apart or break into pieces",
+	},
+	{
+		term: "cline",
+		definition:
+			"A gradual change in a character or feature across the distributional range of a species or population, usually correlated with an environmental or geographic transition.",
+	},
+	{
+		term: "Sintering",
+		definition:
+			"or frittage is the process of compacting and forming a solid mass of material by pressure or heat without melting it to the point of liquefaction.",
+	},
+	{
+		term: "exarchate",
+		definition:
+			"The office, dignity, or administration of an exarch, or the territory ruled by an exarch",
+	},
+	{
+		term: "irredentism",
+		definition:
+			"a policy of advocating the restoration to a country of any territory formerly belonging to it.",
+	},
+	{
+		term: "reify",
+		definition:
+			"(also known as concretism, hypostatization, or the fallacy of misplaced concreteness) is a fallacy of ambiguity, when an abstraction (abstract belief or hypothetical construct) is treated as if it were a concrete real event or physical entity.[1][2] In other words, it is the error of treating something that is not concrete, such as an idea, as a concrete thing. A common case of reification is the confusion of a model with reality: 'the map is not the territory'.",
+		cite: "Wikipedia",
+		citeLink: "https://en.wikipedia.org/wiki/Reification_(fallacy)",
+	},
+];
