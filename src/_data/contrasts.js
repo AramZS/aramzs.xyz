fbe7b05560ef8533e12b4b149c0f8db6a49b8538
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2024-04-05 17:40:07 -0400

    Setting up contrasts and other supporting types

--- src/_data/contrasts.js
+++ src/_data/contrasts.js
@@ -21,9 +21,9 @@ module.exports = {
       "#645c45",
       "#814f1d" // https://www.tumblr.com/smille-c/745203171973070848?source=share
     ],
-    ["#fffada", "#58508c", "#dfb7c0", "#febcab", "#f98b7b", "#262128", "#fed57b"] // https://www.tumblr.com/bir0me/742968247389978624?source=share
-    ["#54c06a", "#f7acc7", "#ae263e", "#573031", "#ebd3c7", "#34252c", "#14a3cb", "#b5bf20", "#ffbc0c", "#f83563"] // https://www.tumblr.com/warakami-vaporwave/746203526570688512?source=share
-    ["#8c7eb7", "#000000", "#7373ad", "#d1c5cf", "#80829c", "#e3c193", "#9fb1cb", "#8a97b6", "#d4a09c", "#687184"] // https://www.tumblr.com/aramzs/124511828648/aramzs-a-clint-barton-kate-bishop-comic-book?source=share
+    ["#fffada", "#58508c", "#dfb7c0", "#febcab", "#f98b7b", "#262128", "#fed57b"], // https://www.tumblr.com/bir0me/742968247389978624?source=share
+    ["#54c06a", "#f7acc7", "#ae263e", "#573031", "#ebd3c7", "#34252c", "#14a3cb", "#b5bf20", "#ffbc0c", "#f83563"], // https://www.tumblr.com/warakami-vaporwave/746203526570688512?source=share
+    ["#8c7eb7", "#000000", "#7373ad", "#d1c5cf", "#80829c", "#e3c193", "#9fb1cb", "#8a97b6", "#d4a09c", "#687184"], // https://www.tumblr.com/aramzs/124511828648/aramzs-a-clint-barton-kate-bishop-comic-book?source=share
     ["#e6504c", "#221e19", "#fbc67f"] // https://www.tumblr.com/zegalba/746226453402796032?source=share
 	],
 };
---
+++ src/content/amplify/index.md
@@ -0,0 +1,12 @@
+---
+title: Amplify
+draft: false
+growthStage: evergreen
+description: Information about amplification on this site.
+tags:
+  - Blogging
+---
+
+# Amplify
+
+This section is for amplifying content posted by other people and on other sites. Think of it as somewhere between a link post and a reblog. 
