96a0b54f1e4bcf1b84646f4597e2fe0c43e0ffbe
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2024-04-07 11:34:44 -0400

    Setting the stage for amplify posts

--- bin/enrichers/pocket.js
+++ bin/enrichers/pocket.js
@@ -4,23 +4,43 @@ const slugger = require("../slugger");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
+const capitalize = (word) => {
+  const capitalized = word.charAt(0).toUpperCase()
++ word.slice(1)
+  return capitalized;
+}
 const transformExportToJSON = (data) => {
   const dom = new JSDOM(data);
   const window = dom.window;
 	const document = window.document;
+  //console.log(data);
 	//const customElements = window.customElements;
 	//const HTMLElement = window.HTMLElement;
   var linkList = [...document.querySelectorAll('li')].map(el => { 
-    let aChild = el.firstChild; 
-    return { 
+    console.log(el, el.outerHTML, el.firstChild.tagName, el.querySelector('A') )
+    if (el.firstChild.tagName !== 'A') return;
+    let aChild = el.querySelector('A'); 
+    let dateString =  aChild.getAttribute('time_added');
+    let dateObj = new Date(parseInt(dateString) * 1000);
+          // Generate a file-slug YYYY-MM-DD string from the date
+          let date = dateObj;
+          let year = date.getFullYear();
+          let month = String(date.getMonth() + 1).padStart(2, "0");
+          let day = String(date.getDate()).padStart(2, "0");
+          let dateFileString = `${year}-${month}-${day}`;
+    let dataSet = { 
       link: aChild.href, 
-      date: new Date(aChild.getAttribute('time_added')).toISOString(), 
-      tags: aChild.getAttribute('tags').split(','),
-      title: aChild.innerText,
-      content: '' 
+      date: dateObj.toISOString(), 
+      tags: aChild.getAttribute('tags').split(',').filter(e => e).map(tag => capitalize(tag)),
+      title: aChild.textContent,
+      content: '',
+      isBasedOn: aChild.href,
+      slug: slugger(dateFileString + "-" + aChild.textContent),
     }
+    console.log('dataset', dataSet);
+    return dataSet;
   });
-  return linkList;
+  return linkList.filter(e => e);
 };
 
 const getPocketExport = () => {
--- src/content/amplify/index.md
+++ src/content/amplify/index.md
@@ -5,6 +5,7 @@ growthStage: evergreen
 description: Information about amplification on this site.
 tags:
   - Blogging
+permalink: /amplify/index.html
 ---
 
 This Amplify section is for amplifying content posted by other people and on other sites. Think of it as somewhere between a link post and a reblog. It pulls together posts that I think are worth sharing, but where I don't have much that I want to write about them.
