8c7f12b9c8fb60ecdefb0ed172765eb27993680f
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2024-04-16 19:25:29 -0400

    chore: adjust title

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -3,7 +3,7 @@ module.exports = {
 		ref: process.env.COMMIT_REF || null,
 		url: process.env.REPOSITORY_URL,
 	},
-	title: "Aram ZS | XYZ Hub",
+	title: "Aram ZS | Digital Garden",
 	description: "Microblog and feed from Aram Zucker-Scharff.",
 	url: "https://aramzs.xyz",
 	feedUrl: "https://aramzs.xyz/writing/feed.xml",
