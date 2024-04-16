670238dff17e59e25a2d7a51e2d370a60daade01
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2024-04-16 19:24:52 -0400

    bugfix: Don't write a file that hasn't changed

--- bin/json-to-markdown.js
+++ bin/json-to-markdown.js
@@ -1,6 +1,35 @@
 const matter = require("gray-matter");
 const slugger = require("./slugger");
 const fs = require("fs");
+
+// From: https://stackoverflow.com/questions/25456013/javascript-deepequal-comparison
+function deepEqual(obj1, obj2) {
+
+  if(obj1 === obj2) // it's just the same object. No need to compare.
+      return true;
+
+  if(isPrimitive(obj1) && isPrimitive(obj2)) // compare primitives
+      return obj1 === obj2;
+
+  if(Object.keys(obj1).length !== Object.keys(obj2).length)
+      return false;
+
+  // compare objects with same number of keys
+  for(let key in obj1)
+  {
+      if(!(key in obj2)) return false; //other object doesn't have this prop
+      if(!deepEqual(obj1[key], obj2[key])) return false;
+  }
+
+  return true;
+}
+
+//check if value is primitive
+function isPrimitive(obj)
+{
+  return (obj !== Object(obj));
+}
+
 /**
  * Take a string that has a YAML block in it and
  * return that YAML block as a JS Object
@@ -115,6 +144,10 @@ const processObjectToMarkdown = (titleProp, contentProp, pathString, obj) => {
 			const newData = { ...obj, ...oldData.data };
 			const newContent =
 				oldData.content.length > 1 ? oldData.content : content;
+      // Don't touch the file if we don't need to.
+      if (newContent == content && deepEqual(newData, oldData.data)) {
+        return true;
+      }
 			return writeDataToMarkdown(
 				pathString,
 				fileSlug,
