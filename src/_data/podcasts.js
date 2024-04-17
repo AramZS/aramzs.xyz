cf2847fc0c91824c98f9d9e3971a1a874482865d
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2024-04-16 23:22:16 -0400

    Updates to content

--- src/_data/podcasts.js
+++ src/_data/podcasts.js
@@ -3,7 +3,7 @@ module.exports = [
     id: 'hazard',
     type: 'podcast',
     name: 'Hazard Podcast',
-    artist: 'Jordan Gass-Poore',
+    artist: 'Jordan Gass-Poore\'',
     image: '/img/listening/podcasts/Hazard-Logo_No-Words.png',
     url: 'https://hazardpodcast.com/',
     feed: 'https://hazardpodcast.com/rss/podcast/index.xml',
