44d32fba20296121b4e620a1d1c5210c89a6e02c
Aram Zucker-Scharff <aramzs@hacktext.com>
2024-11-24 16:38:04 -0500

    Excluding more amplifies to make the build work

--- .eleventy.js
+++ .eleventy.js
@@ -259,6 +259,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add("src/content/amplify/2024-05**");    eleventyConfig.ignores.add("src/content/amplify/2024-06**");
   eleventyConfig.ignores.add("src/content/amplify/2024-07**");
   eleventyConfig.ignores.add("src/content/amplify/2024-08**");
+  eleventyConfig.ignores.add("src/content/amplify/2024-09**");
   // Dev Time Build Ignores 
   if (process.env.IS_LOCAL === "true"){
     //eleventyConfig.ignores.add("src/content/resources/film/[e-t]**");
