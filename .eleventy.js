afc6258ef46ead6df7119dbfdc400e7696f251c1
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2025-10-19 20:34:59 +0200

    Attempt to add back last month's amplify

--- .eleventy.js
+++ .eleventy.js
@@ -314,8 +314,16 @@ import("@photogabble/eleventy-plugin-font-subsetting").then(({ fontSubsetter }) 
   eleventyConfig.ignores.add("src/content/amplify/2022/**");
   eleventyConfig.ignores.add("src/content/amplify/2023/**");
   eleventyConfig.ignores.add("src/content/amplify/2024/**");
-  eleventyConfig.ignores.add("src/content/amplify/2025/0*/**");
-  // Dev Time Build Ignores 
+  // eleventyConfig.ignores.add("src/content/amplify/2025/0*/**");
+  eleventyConfig.ignores.add("src/content/amplify/2025/01/**");
+  eleventyConfig.ignores.add("src/content/amplify/2025/02/**");
+  eleventyConfig.ignores.add("src/content/amplify/2025/03/**");
+  eleventyConfig.ignores.add("src/content/amplify/2025/04/**");
+  eleventyConfig.ignores.add("src/content/amplify/2025/05/**");
+  eleventyConfig.ignores.add("src/content/amplify/2025/06/**");
+  eleventyConfig.ignores.add("src/content/amplify/2025/07/**");
+  eleventyConfig.ignores.add("src/content/amplify/2025/08/**");
+  // Dev Time Build Ignores
   if (process.env.IS_LOCAL === "true"){
     //eleventyConfig.ignores.add("src/content/resources/film/[e-t]**");
     //eleventyConfig.ignores.add("src/content/resources/film/b[u-z]**");
